2c4970a7a8cb9ea83896544ee20bbeed9df6436b
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-20 13:34:10 +0100

    jd-ticketing-project-mvc

---
+++ src/main/java/com/cybertek/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("userCreate")
+    public String userCreate(Model model){
+
+        return "userCreate";
+
+    }
+
+}
