be57934d9915f91d518aa759b29d3ea7fb272fac
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-20 13:26:31 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/model/Manager.java
+++ src/main/java/com/cybertek/model/Manager.java
@@ -1,4 +1,24 @@
 package com.cybertek.model;
 
+import com.cybertek.enums.Gender;
+import com.cybertek.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Manager {
+
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private Role role;
+
 }
