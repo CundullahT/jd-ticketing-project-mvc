33dead4b555b51f607c17a7844b866d032e37c1e
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-21 17:27:14 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/entity/BaseEntity.java
+++ src/main/java/com/cybertek/entity/BaseEntity.java
@@ -18,7 +18,7 @@ public class BaseEntity {
     private Long id;
     private LocalDateTime insertDateTime;
     private Long insertUserId;
-    private LocalDateTime lastUpdateTime;
+    private LocalDateTime lastUpdateDateTime;
     private Long lastUpdateUserId;
 
 }
