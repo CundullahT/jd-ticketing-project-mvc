f80f8d9c82f0a770e0dc9f8e2f93b65e18f4fe4c
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-21 17:00:51 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.cybertek.controller;
 
-import com.cybertek.datagenerator.DataGenerator;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
 
 @Controller
 @RequestMapping("/user")
