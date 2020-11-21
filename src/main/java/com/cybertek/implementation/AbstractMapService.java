872a0b5c254c85b184da6c621721080ef23f786d
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-21 20:43:46 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/implementation/AbstractMapService.java
+++ src/main/java/com/cybertek/implementation/AbstractMapService.java
@@ -1,10 +1,33 @@
 package com.cybertek.implementation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractMapService<ID, T> {
 
     protected Map<ID, T> map = new HashMap<>();
+
+    T save(ID id, T t){
+        map.put(id,t);
+        return t;
+    }
+
+    List<T> findAll(){
+        return new ArrayList<>(map.values());
+    }
+
+    T findByID(ID id){
+        return map.get(id);
+    }
+
+    void deleteByID(ID id){
+        map.remove(id);
+    }
+
+    void delete(T t){
+        map.entrySet().removeIf(entry -> entry.getValue().equals(t));
+    }
 
 }
