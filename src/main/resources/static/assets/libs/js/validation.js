e8927b455e6ffe464b851d1d62d8543eff66e98c
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-21 16:56:52 +0100

    jd-ticketing-project-mvc

--- src/main/resources/static/assets/libs/js/validation.js
+++ src/main/resources/static/assets/libs/js/validation.js
@@ -1,25 +1,18 @@
-
-  function Validate() {
-      var password = document.getElementById("pass1").value;
-      var confirmPassword = document.getElementById("pass2").value;
-      if (password != confirmPassword) {
-          alert("Passwords do not match.");
-          return false;
-      }
-      return true;
-  }
-  
-  
-  (function () {  
-          'use strict';  
-          window.addEventListener('load', function () {  
-              var form = document.getElementById('needs-validation');  
-              form.addEventListener('submit', function (event) {  
-                  if (form.checkValidity() === false) {  
-                      event.preventDefault();  
-                      event.stopPropagation();  
-                  }  
-                  form.classList.add('was-validated');  
-              }, false);  
-          }, false);  
-  })(); 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
