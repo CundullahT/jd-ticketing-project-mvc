991b6c29064dd5f5de999a4978319593193fe748
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-21 17:21:58 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/enums/Gender.java
+++ src/main/java/com/cybertek/enums/Gender.java
@@ -2,6 +2,16 @@ package com.cybertek.enums;
 
 public enum Gender {
 
-    MALE, FEMALE;
+    MALE("Male"), FEMALE("Female");
+
+    private final String value;
+
+    private Gender(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
 
 }
