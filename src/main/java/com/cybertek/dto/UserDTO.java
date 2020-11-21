c3a64ca4930e65c358aecddb4c0a693029cdfd82
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-21 17:30:55 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/dto/UserDTO.java
+++ src/main/java/com/cybertek/dto/UserDTO.java
@@ -1,7 +1,17 @@
 package com.cybertek.dto;
 
+import com.cybertek.entity.Role;
+import com.cybertek.enums.Gender;
+
 public class UserDTO {
 
-
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private RoleDTO role;
+    private Gender gender;
 
 }
