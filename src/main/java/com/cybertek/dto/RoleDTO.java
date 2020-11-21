e385285cd7692a00992e27f21d23e1f6b9abd862
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-21 17:31:56 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/dto/RoleDTO.java
+++ src/main/java/com/cybertek/dto/RoleDTO.java
@@ -1,5 +1,14 @@
 package com.cybertek.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class RoleDTO {
 
     private Long id;
