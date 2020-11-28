dc5e861a0445c439d6e8074ecbdd585c7f2467e6
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-28 19:28:47 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.cybertek.controller;
 
+import com.cybertek.dto.TaskDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TaskController {
 
     @GetMapping("create")
-    public String createTask(){
+    public String createTask(Model model){
+
+        model.addAttribute("task",new TaskDTO());
 
         return "/task/create";
 
---
+++ src/main/java/com/cybertek/dto/TaskDTO.java
@@ -0,0 +1,28 @@
+package com.cybertek.dto;
+
+import com.cybertek.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TaskDTO {
+
+    private Long id;
+    private ProjectDTO project;
+    private UserDTO assignedEmployee;
+    private String taskSubject;
+    private String taskDetail;
+    private Status taskStatus;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate assignedDate;
+
+}
