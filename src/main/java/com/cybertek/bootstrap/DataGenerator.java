797e2b48e043d820965b27e92e7cc012cb5b44b7
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-28 16:16:45 +0100

    jd-ticketing-project-mvc

--- src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -40,21 +40,29 @@ public class DataGenerator implements CommandLineRunner {
         roleService.save(employeeRole);
 
         UserDTO user1 = new UserDTO("John", "Kesy",
-                "admin@cybertek.com","abc",true,"7459684532",managerRole, Gender.MALE);
-        UserDTO user2 = new UserDTO("Delisa", "Norre",
-                "T001@cybertek.com","123",true,"8567412358", managerRole, Gender.FEMALE);
+                "john@cybertek.com", "Abc1", true, "7459684532", managerRole, Gender.MALE);
+        UserDTO user5 = new UserDTO("Mike", "Smith",
+                "mike@cybertek.com", "Abc2", true, "7459684532", adminRole, Gender.MALE);
+        UserDTO user2 = new UserDTO("Delisa",
+                "Norre", "delisa@cybertek.com", "123", true, "8567412358", managerRole, Gender.FEMALE);
         UserDTO user3 = new UserDTO("Craig", "Jark",
-                "P001@cybertek.com","123",true,"7777775566", employeeRole, Gender.MALE);
-        UserDTO user4 = new UserDTO("Shaun", "Hayns",
-                "S001@cybertek.com","123",true,"3256987412", employeeRole, Gender.MALE);
-        UserDTO user5 = new UserDTO("John", "Kesy",
-                "admin2@cybertek.com","abc",true,"7459684532", adminRole, Gender.MALE);
-
+                "craig@cybertek.com", "Abc3", true, "7777775566", employeeRole, Gender.MALE);
+        UserDTO user4 = new UserDTO("Shaun",
+                "Hayns", "shaun@cybertek.com", "Abc4", true, "3256987412", managerRole, Gender.MALE);
+        UserDTO user6 = new UserDTO("Elizebeth",
+                "Loren", "elizebeth@cybertek.com", "Abc4", true, "5306987412", employeeRole, Gender.FEMALE);
+        UserDTO user7 = new UserDTO("Maria",
+                "Ada", "maria@cybertek.com", "Abc4", true, "9996987412", employeeRole, Gender.FEMALE);
+        UserDTO user8 = new UserDTO("Bill",
+                "Matt", "maria@cybertek.com", "Abc4", true, "8881239846", employeeRole, Gender.MALE);
         userService.save(user1);
         userService.save(user2);
         userService.save(user3);
         userService.save(user4);
         userService.save(user5);
+        userService.save(user6);
+        userService.save(user7);
+        userService.save(user8);
 
         ProjectDTO project1 = new ProjectDTO("Spring MVC","PR001",user1, LocalDate.now(),LocalDate.now().plusDays(25),"Creating Controllers", Status.OPEN);
         ProjectDTO project2 = new ProjectDTO("Spring ORM","PR002",user2, LocalDate.now(),LocalDate.now().plusDays(10),"Creating Database", Status.IN_PROGRESS);
